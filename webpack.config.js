d10b9f97f29a61891449b3b5edd03c00af63ecaf
Satoko <satoko@suzuki.email.ne.jp>
2022-11-13 16:16:50 +0900

    index.js path changed

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 
     target: 'node',
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, ''),
         filename: 'index.js'
       },
     
