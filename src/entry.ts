6691f64b3a0f0fa002e858e5cda37627eb8c14d1
Satoko <satoko@suzuki.email.ne.jp>
2022-12-15 13:01:03 +0900

    CommandHookType外だし。bootstrapサイズ大きいので外した。

--- src/entry.ts
+++ src/entry.ts
@@ -2,3 +2,4 @@ export * from './components';
 export { MapMode } from './types/types';
 export * from './util/Commander';
 export { FilterDefine } from './store/operation/operationSlice';
+export { CommandHookType } from './api/useCommand';
