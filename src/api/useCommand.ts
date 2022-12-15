18bf46be7df678b651c2558a87f430d221fd357d
Satoko <satoko@suzuki.email.ne.jp>
2022-12-15 14:08:53 +0900

    外だしAPI追加

--- src/api/useCommand.ts
+++ src/api/useCommand.ts
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { useCallback } from 'react';
 import { RootState, useAppDispatch } from "../store/configureStore";
 import { callApi } from "./api";
-import { api } from '279map-common';
+import { api, UnpointContent } from '279map-common';
 import { registContent, updateContent, linkContentToItem } from '../store/data/dataThunk';
 import useConfirm from "../components/common/confirm/useConfirm";
 
@@ -35,12 +35,24 @@ export function useCommand() {
         return res;
     }, [mapServer]);
 
+    const getUnpointDataAPI = useCallback(async(nextToken?: string) => {
+        const result = await callApi(mapServer, api.GetUnpointDataAPI, {
+            nextToken,
+        });
+        return {
+            contents: result.contents as UnpointContent[],
+            nextToken: result.nextToken,
+        };
+
+    }, [mapServer]);
+
     return {
         confirm,
         registContentAPI,
         updateContentAPI,
         linkContentToItemAPI,
         getSnsPreviewAPI,
+        getUnpointDataAPI,
     }
 }
 export type CommandHookType = ReturnType<typeof useCommand>;
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
 export { default as CategoryBadge } from './CategoryBadge';
 export { default as Button } from './button/Button';
 export { default as Modal, ModalHeader, ModalBody, ModalFooter } from './modal/Modal';
+export { default as ImageRegister } from './image/ImageRegister';
