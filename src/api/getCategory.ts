6eb4ded2d030728ff8003dcce9731831327e820c
Satoko <satoko@suzuki.email.ne.jp>
2022-12-20 11:42:09 +0900

    getCategoryで返却する情報を変更

--- src/api/getCategory.ts
+++ src/api/getCategory.ts
@@ -4,6 +4,12 @@ import { api } from "279map-common";
 import { ContentsTable } from "279map-backend-common/dist/types/schema";
 import { CategoryDefine } from "279map-common";
 
+/**
+ * get the categories which are used in current map.
+ * don't consider about map kind.
+ * @param param0 
+ * @returns categories
+ */
 export const getCategory: APIFunc<void, api.GetCategoryResult> = async({ currentMap }) => {
     if (!currentMap) {
         throw 'mapPageId or mapKind not defined.';
@@ -15,30 +21,23 @@ export const getCategory: APIFunc<void, api.GetCategoryResult> = async({ current
 
     try {
         const sql = `
-            select c.*, i.item_page_id from contents c 
-            inner join items i ON i.content_page_id = c.content_page_id  
-            inner join contents_db_info cdi ON i.contents_db_id = cdi.contents_db_id 
-            where category <> '[]' and cdi.map_page_id = ? and i.map_kind = ?
+            select c.* from contents c 
+            inner join contents_db_info cdi ON c.contents_db_id = cdi.contents_db_id 
+            where category <> '[]' and cdi.map_page_id = ?
         `;
 
-        const [rows] = await con.execute(sql, [mapPageId, mapKind]);
+        const [rows] = await con.execute(sql, [mapPageId]);
         const categoryMap = new Map<string, CategoryDefine>();
-        (rows as (ContentsTable & {item_page_id: string})[]).forEach((row) => {
+        (rows as ContentsTable[]).forEach((row) => {
             const categories = JSON.parse(row.category as string) as string[];
             categories.forEach(category => {
                 if (categoryMap.has(category)) {
-                    categoryMap.get(category)?.contents.push({
-                        content_id: row.content_page_id,
-                        item_id: row.item_page_id,
-                    });
+                    categoryMap.get(category)?.content_ids.push(row.content_page_id);
                 } else {
                     const def = {
                         name: category,
                         color: '',
-                        contents: [{
-                            content_id: row.content_page_id,
-                            item_id: row.item_page_id,
-                        }],
+                        content_ids: [row.content_page_id],
                     } as CategoryDefine;
                     categoryMap.set(category, def);
                 }
