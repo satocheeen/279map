850f5c360aa59748cc0b6b347897e2a5bea2dd81
Satoko <satoko@suzuki.email.ne.jp>
2023-02-14 16:18:45 +0900

    Tooltip外だし

--- 279map-core/src/components/common/index.ts
+++ 279map-core/src/components/common/index.ts
@@ -2,3 +2,4 @@ export { default as CategoryBadge } from './CategoryBadge';
 export { default as Button } from './button/Button';
 export { default as Modal, ModalHeader, ModalBody, ModalFooter } from './modal/Modal';
 export { default as ImageRegister } from './image/ImageRegister';
+export { default as Tooltip} from './tooltip/Tooltip';
