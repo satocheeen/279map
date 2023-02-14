c56359be7b73b4a73b06af4a93dfa0bc77f78074
Satoko <satoko@suzuki.email.ne.jp>
2023-02-14 16:30:37 +0900

    Revert "Tooltip外だし"
    This reverts commit 850f5c360aa59748cc0b6b347897e2a5bea2dd81.

--- 279map-core/src/components/common/index.ts
+++ 279map-core/src/components/common/index.ts
@@ -2,4 +2,3 @@ export { default as CategoryBadge } from './CategoryBadge';
 export { default as Button } from './button/Button';
 export { default as Modal, ModalHeader, ModalBody, ModalFooter } from './modal/Modal';
 export { default as ImageRegister } from './image/ImageRegister';
-export { default as Tooltip} from './tooltip/Tooltip';
