e9177231c9d74ead657961b93fb948d8357da530
Satoko <satoko@suzuki.email.ne.jp>
2023-03-08 10:37:17 +0900

    DBのreadonlyフラグを付けてフロントエンドに返却するように対応

--- 279map-backend-main/src/getContents.ts
+++ 279map-backend-main/src/getContents.ts
@@ -41,6 +41,7 @@ export async function getContents({ param, currentMap }: {param: GetContentsPara
                     anotherMapItemId: another_item_ids.length === 0 ? undefined : another_item_ids[0],  // 複数存在する場合は１つだけ返す
                     isSnsContent,
                     addableChild,
+                    readonly: row.readonly,
                 };
         }
         const getChildren = async(contentId: string): Promise<ContentsDefine[]> => {
