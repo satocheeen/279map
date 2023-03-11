7422d3bf64a4e405f2706ef8e08ca0b3c09b08db
Satoko <satoko@suzuki.email.ne.jp>
2023-03-11 12:08:49 +0900

    スキーマ変更に伴う修正

--- 279map-db/init/2_create_record.sql
+++ 279map-db/init/2_create_record.sql
@@ -3,7 +3,3 @@ SET CHARSET UTF8;
 INSERT INTO 279map_db.map_page_info
 (map_page_id, title, last_edited_time, alias, default_map, use_maps)
 VALUES('testmap', 'Testマップ', '2022-10-17T00:00:00.000', NULL, 'Real', 'Virtual,Real');
-
-INSERT INTO 279map_db.contents_db_info
-(map_page_id, contents_db_id, kind, last_edited_time, sync_service_name)
-VALUES('testmap', 'location', 'Item', '2022-10-17T00:00:00.000Z', 'TEST_MAP');
