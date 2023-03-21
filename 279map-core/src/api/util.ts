b062e9271d1403cc93e86c83634e4a71bb0e2d58
Satoko <satoko@suzuki.email.ne.jp>
2023-03-21 17:39:55 +0900

    coreから呼び出し元にアクセス可能な地図一覧を返すI/Fを用意

--- 279map-core/src/api/util.ts
+++ 279map-core/src/api/util.ts
@@ -1,7 +1,27 @@
-import { ConfigAPI } from "tsunagumap-api";
+import { ConfigAPI, GetMapListAPI } from "tsunagumap-api";
 import { callApi } from "./api";
 import { ServerInfo } from '../types/types';
 import { ServerConfig } from "../279map-common";
+
+/**
+ * ユーザがアクセス可能な地図一覧を返す
+ * @param host 
+ * @returns 
+ */
+export async function getAccessableMapList(host: string) {
+    const mapServer = {
+        domain: host,
+    } as ServerInfo;
+    try {
+        const result = await callApi(mapServer, GetMapListAPI, undefined);
+        return result;
+
+    } catch(e) {
+        console.warn('get accessable maplist failed.', e);
+        throw new Error('get accessable maplist failed.', { cause: e});
+    }
+
+}
 
 /**
  * サーバーの認証方式について呼び出し元に返すための関数
