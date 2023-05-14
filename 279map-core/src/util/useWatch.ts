8fab5a5aa3a09a64e23aaea089547b2a458ece20
Satoko <satoko@suzuki.email.ne.jp>
2023-05-14 14:51:45 +0900

    コメント修正

--- 279map-core/src/util/useWatch.ts
+++ 279map-core/src/util/useWatch.ts
@@ -1,7 +1,9 @@
 import { useEffect, DependencyList } from 'react';
 
 /**
- * コンポーネントマウント時のフック
+ * 指定した値が変更した際のフック。
+ * useEffectは用途が多いので、明示的にフックを用意することで、
+ * 使用目的を明確にしている。
  */
 export function useWatch(process: () => void, deps: DependencyList) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
