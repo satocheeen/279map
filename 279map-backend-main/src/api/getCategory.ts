81cd14f795e56c7171882bfd55a0e19d55a50205
Satoko <satoko@suzuki.email.ne.jp>
2023-05-14 16:26:15 +0900

    カテゴリ情報取得時に、現在の地図種別で使用されているかどうかを付与

--- 279map-backend-main/src/api/getCategory.ts
+++ 279map-backend-main/src/api/getCategory.ts
@@ -3,11 +3,13 @@ import { ConnectionPool } from "..";
 import { CurrentMap, schema } from "279map-backend-common";
 import { GetCategoryResult } from "../../279map-api-interface/src";
 import { CategoryDefine } from "279map-backend-common";
+import { getLogger } from "log4js";
+
+const apiLogger = getLogger('api');
 
 /**
  * get the categories which are used in current map.
- * don't consider about map kind.
- * @param param0 
+ * @param currentMap 
  * @returns categories
  */
 export async function getCategory(currentMap: CurrentMap): Promise<GetCategoryResult> {
@@ -20,10 +22,11 @@ export async function getCategory(currentMap: CurrentMap): Promise<GetCategoryRe
     const con = await ConnectionPool.getConnection();
 
     try {
+        // コンテンツで使用されているカテゴリを取得
         const sql = `
             select c.* from contents c 
             inner join map_datasource_link mdl on mdl.data_source_id = c.data_source_id 
-            where category <> '[]' and mdl.map_page_id = ?
+            where category is not NULL and mdl.map_page_id = ?
         `;
 
         const [rows] = await con.execute(sql, [mapPageId]);
@@ -44,9 +47,35 @@ export async function getCategory(currentMap: CurrentMap): Promise<GetCategoryRe
                             id: row.content_page_id,
                             dataSourceId: row.data_source_id,
                         }],
+                        using: false,
                     } as CategoryDefine;
                     categoryMap.set(category, def);
                 }
+            })
+        });
+
+        // 指定地図上のアイテムで使われているものを取得
+        const itemSql = `
+            select c.* from contents c 
+            where exists (
+                select icl.* from item_content_link icl 
+                inner join items i on i.item_page_id = icl.item_page_id and i.data_source_id = icl.item_datasource_id 
+                inner join map_datasource_link mdl on mdl.data_source_id = i.data_source_id 
+                where icl.content_page_id = c.content_page_id and icl.content_datasource_id  = c.data_source_id
+                and category is not NULL 
+                and mdl.map_page_id = ? and i.map_kind = ?
+            )
+        `;
+        const [itemSqlRows] = await con.execute(itemSql, [mapPageId, mapKind]);
+        (itemSqlRows as schema.ContentsTable[]).forEach((row) => {
+            const categories = (row.category ?? []) as string[];
+            categories.forEach(category => {
+                const target = categoryMap.get(category)
+                if (!target) {
+                    apiLogger.warn('想定外', category);
+                    return;
+                }
+                target.using = true;
             })
         });
 
