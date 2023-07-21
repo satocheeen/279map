29c35115396f6dd4e66e4ffcb277ec70a3350fd4
Satoko <satoko@suzuki.email.ne.jp>
2023-07-21 17:06:55 +0900

    interface定義誤り修正

--- 279map-backend-common/src/auth/AuthManagementInterface.ts
+++ 279map-backend-common/src/auth/AuthManagementInterface.ts
@@ -6,14 +6,14 @@ export type MapInfo = {
 }
 
 export abstract class AuthManagementInterface {
-    abstract initialize: () => Promise<void>;
+    abstract initialize(): Promise<void>;
 
     /**
      * 指定のユーザがユーザ登録している地図一覧を返す
      * @param userId 
      * @returns 地図id一覧
      */
-    abstract getUserMapList: (userId: string) => Promise<string[]>;
+    abstract getUserMapList(userId: string): Promise<string[]>;
 
     /**
      * 指定のユーザの指定の地図での権限情報を返す
@@ -21,5 +21,5 @@ export abstract class AuthManagementInterface {
      * @param mapId 
      * @return ユーザ情報。該当するデータが存在しない場合、null。
      */
-    abstract getUserInfoOfTheMap: (userId: string, mapId: string) => Promise<MapInfo|undefined>;
+    abstract getUserInfoOfTheMap(userId: string, mapId: string): Promise<MapInfo|undefined>;
 }
