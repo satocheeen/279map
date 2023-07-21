40757a59654aaebded539b1d457861bcd4dd9ffc
Satoko <satoko@suzuki.email.ne.jp>
2023-07-21 16:57:38 +0900

    AuthManagementInterfaceを追加

---
+++ 279map-backend-common/src/auth/AuthManagementInterface.ts
@@ -0,0 +1,25 @@
+import { Auth } from "279map-common";
+
+export type MapInfo = {
+    auth_lv: Auth;
+    name: string;
+}
+
+export default abstract class authManagementInterface {
+    abstract initialize: () => Promise<void>;
+
+    /**
+     * 指定のユーザがユーザ登録している地図一覧を返す
+     * @param userId 
+     * @returns 地図id一覧
+     */
+    abstract getUserMapList: (userId: string) => Promise<string[]>;
+
+    /**
+     * 指定のユーザの指定の地図での権限情報を返す
+     * @param userId 
+     * @param mapId 
+     * @return ユーザ情報。該当するデータが存在しない場合、null。
+     */
+    abstract getUserInfoOfTheMap: (userId: string, mapId: string) => Promise<MapInfo|undefined>;
+}
--- 279map-backend-common/src/index.ts
+++ 279map-backend-common/src/index.ts
@@ -4,6 +4,7 @@
 export * as api from './api';
 export * as sns from './sns';
 export * from './types';
+export * from './auth';
 
 export * from './utility';
 export * from '279map-common';
