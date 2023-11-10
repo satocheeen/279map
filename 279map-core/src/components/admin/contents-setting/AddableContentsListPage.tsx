2e4d40a3da96742e8db2f5c7a8d83e214786d728
Satoko <satoko@suzuki.email.ne.jp>
2023-11-10 10:41:55 +0900

    mainマージに伴うエラー修正

--- 279map-core/src/components/admin/contents-setting/AddableContentsListPage.tsx
+++ 279map-core/src/components/admin/contents-setting/AddableContentsListPage.tsx
@@ -4,7 +4,6 @@ import { GetLinkableContentsAPI } from 'tsunagumap-api';
 import styles from './DefaultContentsSettingModal.module.scss';
 import { useAtom } from 'jotai';
 import { modalSpinnerAtom } from '../../common/modal/Modal';
-import { dataSourcesAtom } from '../../../store/datasource';
 import { useWatch } from '../../../util/useWatch2';
 
 type Props = {
@@ -15,23 +14,18 @@ type Props = {
 export type AddableContentItem = {
     datasourceId: string;
     name: string;
-    virtual?: boolean;
-    real: {[itemDatasourceId: string]: boolean};
+    checked?: boolean;
 }
 export default function AddableContentsListPage(props: Props) {
     const { callApi } = useApi();
-    const [list, setList] = useState<AddableContentItem[]>([]);
+    const [ list, setList ] = useState<AddableContentItem[]>([]);
     const [, setModalSpinner] = useAtom(modalSpinnerAtom);
 
     useWatch(list, 
         useCallback((oldVal, newVal) => {
             if (!props.onChange) return;
             // チェックされたものに絞る
-            const targets = newVal.filter(item => {
-                if (item.virtual) return true;
-                return Object.values(item.real).some(v => v);
-            })
-
+            const targets = newVal.filter(item => item.checked);
             props.onChange(targets);
         }, [props])
     )
@@ -50,61 +44,31 @@ export default function AddableContentsListPage(props: Props) {
         })
     }, [callApi, setModalSpinner])
 
-    const onVirtualCheck = useCallback((index: number, val: boolean) => {
+    const handleChecked = useCallback((index: number, val: boolean) => {
         setList(cur => {
             const newList = structuredClone(cur);
-            newList[index].virtual = val;
+            newList[index].checked = val;
             return newList;
         });
     }, [])
-
-    const onRealCheck = useCallback((index: number, itemDatasourceId: string, val: boolean) => {
-        setList(cur => {
-            const newList = structuredClone(cur);
-            newList[index].real[itemDatasourceId] = val;
-            return newList;
-        })
-    }, []);
-    
-
-    const [dataSources] = useAtom(dataSourcesAtom);
-    const realMapLayers = useMemo(() => {
-        return dataSources
-            .filter(ds => ds.itemContents.RealItem);
-    }, [dataSources]);
 
     return (
         <div className={styles.TableContainer}>
             <table className={styles.Table}>
                 <thead>
                     <tr>
-                        <th rowSpan={2}>コンテンツ名</th>
-                        <th rowSpan={2}>村マップ</th>
-                        <th colSpan={realMapLayers.length}>世界地図</th>
-                    </tr>
-                    <tr>
-                        {realMapLayers.map(l => {
-                            return (
-                                <th key={l.dataSourceId}>{l.name}</th>
-                            )
-                        })}
+                        <th></th>
+                        <th>コンテンツ名</th>
                     </tr>
                 </thead>
                 <tbody>
                     {list.map((ds, index) => {
                         return (
                             <tr key={ds.datasourceId}>
-                                <td>{ds.name}</td>
                                 <td>
-                                    <input type='checkbox' checked={ds.virtual ?? false} onChange={(evt) => onVirtualCheck(index, evt.target.checked)} />
+                                    <input type='checkbox' checked={ds.checked ?? false} onChange={(evt) => handleChecked(index, evt.target.checked)} />
                                 </td>
-                                {realMapLayers.map(l => {
-                                    return (
-                                        <td key={l.dataSourceId}>
-                                            <input type='checkbox' checked={ds.real[l.dataSourceId] ?? false} onChange={(evt) => onRealCheck(index, l.dataSourceId, evt.target.checked)} />
-                                        </td>
-                                    )
-                                })}
+                                <td>{ds.name}</td>
                             </tr>
                         )
                     })}
--- 279map-core/src/components/admin/contents-setting/CurrentContentsListPage.tsx
+++ 279map-core/src/components/admin/contents-setting/CurrentContentsListPage.tsx
@@ -1,83 +1,27 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import styles from './DefaultContentsSettingModal.module.scss';
 import { useAtom } from 'jotai';
-import { dataSourcesAtom } from '../../../store/datasource';
-import { Button } from '../../common';
+import { contentDataSourcesAtom } from '../../../store/datasource';
 
 type Props = {
 }
 
-type ContentRecord = {
-    id: string;
-    name: string;
-    virtual: boolean;   // 村マップのアイテムに紐づけ可能な場合、true
-    real: {
-        id: string;
-        name: string;
-    }[];     // 世界地図のアイテム（レイヤ）に紐づけ可能なアイテムデータソースの一覧
-}
-
 export default function CurrentContentsListPage(props: Props) {
-    const [dataSources] = useAtom(dataSourcesAtom);
-
-    const contentRecords = useMemo(() => {
-        return dataSources.filter(ds => {
-            return ds.itemContents.Content;
-        }).map((ds): ContentRecord => {
-            const virtual = dataSources.some(ds2 => {
-                return ds2.itemContents.VirtualItem?.linkableContents.some(lc => lc.contentDatasourceId === ds.dataSourceId)
-            });
-            const real = dataSources.filter(ds2 => {
-                return ds2.itemContents.RealItem?.linkableContents.some(lc => lc.contentDatasourceId === ds.dataSourceId);
-            }).map(ds2 => ({
-                id: ds2.dataSourceId,
-                name: ds2.name,
-            }));
-            return {
-                id: ds.dataSourceId,
-                name: ds.name,
-                virtual,
-                real,
-            }
-        })
-    }, [dataSources]);
-
-    const realMapLayers = useMemo(() => {
-        return dataSources
-            .filter(ds => ds.itemContents.RealItem);
-    }, [dataSources]);
+    const [contentDataSources] = useAtom(contentDataSourcesAtom);
 
     return (
         <div className={styles.TableContainer}>
             <table className={styles.Table}>
                 <thead>
                     <tr>
-                        <th rowSpan={2}>コンテンツ名</th>
-                        <th rowSpan={2}>村マップ</th>
-                        <th colSpan={realMapLayers.length}>世界地図</th>
-                    </tr>
-                    <tr>
-                        {realMapLayers.map(l => {
-                            return (
-                                <th key={l.dataSourceId}>{l.name}</th>
-                            )
-                        })}
+                        <th>コンテンツ名</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {contentRecords.map(ds => {
+                    {contentDataSources.map(ds => {
                         return (
-                            <tr key={ds.id}>
+                            <tr key={ds.dataSourceId}>
                                 <td>{ds.name}</td>
-                                <td>{ds.virtual ? '✔' : ''}</td>
-                                {realMapLayers.map(l => {
-                                    return (
-                                        <td key={l.dataSourceId}>
-                                            {ds.real.some(r => r.id === l.dataSourceId) ? '✔' : ''}
-                                        </td>
-                                    )
-                                })}
-
                             </tr>
                         )
                     })}
