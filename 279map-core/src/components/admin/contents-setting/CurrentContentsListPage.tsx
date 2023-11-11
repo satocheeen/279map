cb609ced9cecaf8d9a0a05dcc3aa9507f1378b7a
Satoko <satoko@suzuki.email.ne.jp>
2023-11-11 11:28:08 +0900

    表内に削除ボタンを用意

--- 279map-core/src/components/admin/contents-setting/CurrentContentsListPage.tsx
+++ 279map-core/src/components/admin/contents-setting/CurrentContentsListPage.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import styles from './DefaultContentsSettingModal.module.scss';
 import { useAtom } from 'jotai';
 import { contentDataSourcesAtom } from '../../../store/datasource';
+import PopupMenuIcon from '../../popup/PopupMenuIcon';
+import { MdDelete } from 'react-icons/md';
+import { DataSourceKindType } from '279map-common';
 
 type Props = {
 }
 
 export default function CurrentContentsListPage(props: Props) {
     const [contentDataSources] = useAtom(contentDataSourcesAtom);
+    const handleDelete = useCallback(() => {
+
+    }, []);
 
     return (
         <div className={styles.TableContainer}>
@@ -15,6 +21,7 @@ export default function CurrentContentsListPage(props: Props) {
                 <thead>
                     <tr>
                         <th>コンテンツ名</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -22,6 +29,13 @@ export default function CurrentContentsListPage(props: Props) {
                         return (
                             <tr key={ds.dataSourceId}>
                                 <td>{ds.name}</td>
+                                <td>
+                                    {!(ds.kind === DataSourceKindType.Content && ds.disableUnlinkMap) &&
+                                        <PopupMenuIcon tooltip="削除" onClick={handleDelete}>
+                                            <MdDelete />
+                                        </PopupMenuIcon>
+                                    }
+                                </td>
                             </tr>
                         )
                     })}
