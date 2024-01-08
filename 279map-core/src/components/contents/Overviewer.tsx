d8283b1fb0a5704b30e7bb20eb812cfb6c8d8bae
Satoko <satoko@suzuki.email.ne.jp>
2024-01-08 12:58:12 +0900

    長いコンテンツは折り畳み表示する対応

--- 279map-core/src/components/contents/Overviewer.tsx
+++ 279map-core/src/components/contents/Overviewer.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import styles from './Overviewer.module.scss';
 import reactStringReplace from 'react-string-replace';
 
@@ -12,6 +12,23 @@ type Props = {
 }
 
 export default function Overviewer(props: Props) {
+    const myRef = useRef<HTMLDivElement>(null);
+    const descRef = useRef<HTMLDivElement>(null);
+    const [isOver, setIsOver] = useState(false);    // 文章が長い場合、true
+    const [ showOver, setShowOver ] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            // 文字が収まりきっているかチェック
+            if (myRef.current === null || descRef.current === null) {
+                return;
+            }
+            const myHeight = myRef.current.getBoundingClientRect().height;
+            const descHeight = descRef.current.getBoundingClientRect().height;
+            setIsOver(myHeight < descHeight);
+        }, 100);
+    }, []);
+
     const overview = useMemo(() => {
         // 改行
         let i = 0;
@@ -34,7 +51,22 @@ export default function Overviewer(props: Props) {
 
     }, [props.overview]);
 
+    const onToggle = useCallback(() => {
+        setShowOver((cur) => !cur);
+    }, [])
+
     return (
-        <p className={styles.Overview}>{overview}</p>
+        <div className={styles.Container}>
+            <div className={`${styles.Overview} ${showOver ? '' : styles.Close}`} ref={myRef}>
+                <div ref={descRef}>
+                    {overview}
+                </div>
+            </div>
+            {isOver &&
+                <div className={styles.Toggle} onClick={onToggle}>
+                    {showOver ? '▲閉じる' : '▼続きを読む'}
+                </div>
+            }
+        </div>
     );
 }
