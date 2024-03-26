84e8e783879d61ca0ca9a01ad72891a2e34988aa
Satoko <satoko@suzuki.email.ne.jp>
2024-03-26 18:43:25 +0900

    型定義誤り修正

--- 279map-core/src/types/types.ts
+++ 279map-core/src/types/types.ts
@@ -193,13 +193,13 @@ export interface TsunaguMapHandler {
      * 指定のアイテム配下のコンテンツを取得する
      * @param itemId 
      */
-    loadContentsInItem(itemId: DataId, changeListener?: (contentId: DataId, operation: Operation) => void): Promise<LoadContentsResult<T>>;
+    loadContentsInItem(itemId: DataId, changeListener?: (contentId: DataId, operation: Operation) => void): Promise<LoadContentsResult>;
 
     /**
      * 指定のコンテンツを取得する
      * @param contentIds 
      */
-    loadContents(contentIds: DataId[], changeListener?: (contentId: DataId, operation: Operation) => void): Promise<LoadContentsResult<T>>;
+    loadContents(contentIds: DataId[], changeListener?: (contentId: DataId, operation: Operation) => void): Promise<LoadContentsResult>;
 
     /**
      * 指定の画像データ(Base64)を取得する
