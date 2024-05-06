2907a776cb44598d9404cd37fe59acaf2708ae61
Satoko <satoko@suzuki.email.ne.jp>
2024-05-06 16:36:44 +0900

    getEvents実装

--- 279map-backend-main/src/getEvents.ts
+++ 279map-backend-main/src/getEvents.ts
@@ -3,6 +3,7 @@ import { getLogger } from "log4js";
 import { CurrentMap } from "../279map-backend-common/src";
 import { EventContent, EventDefine, QueryGetEventArgs } from "./graphql/__generated__/types";
 import { QueryResolverReturnType } from "./graphql/type_utility";
+import { DatasourceLocationKindType, MapKind } from "./types-common/common-types";
 
 const logger = getLogger('api');
 export async function getEvents(param: QueryGetEventArgs, currentMap: CurrentMap): QueryResolverReturnType<'getEvent'> {
@@ -23,11 +24,7 @@ export async function getEvents(param: QueryGetEventArgs, currentMap: CurrentMap
             const val = entry[1];
             const contents = val.map((v): EventContent => {
                 return {
-                    id: v.content_data_id,
-                    // id: {
-                    //     id: v.content_page_id,
-                    //     dataSourceId: v.content_datasource_id,
-                    // },
+                    id: v.data_id,
                     date: v.date,
                 }
             });
@@ -46,7 +43,7 @@ export async function getEvents(param: QueryGetEventArgs, currentMap: CurrentMap
 type DateResult = {
     data_source_id: string;
     date: string;
-    content_data_id: string;
+    data_id: string;
 }
 /**
  * 指定の地図上のdateを保持するコンテンツ一覧を返す
@@ -56,44 +53,44 @@ type DateResult = {
 async function getAllDates(currentMap: CurrentMap, dataSourceIds?: string[]): Promise<DateResult[]> {
     if (dataSourceIds && dataSourceIds.length === 0) return [];
 
-    return [];
-    // const con = await ConnectionPool.getConnection();
+    const con = await ConnectionPool.getConnection();
 
-    // try {
-    //     await con.beginTransaction();
+    try {
+        await con.beginTransaction();
 
-    //     const sql = `
-    //     select c.data_source_id, DATE_FORMAT(c.date, '%Y-%m-%d') as date, icl.content_page_id, icl.content_datasource_id from contents c
-    //     inner join map_datasource_link mdl on mdl.data_source_id = c.data_source_id 
-    //     inner join item_content_link icl on icl.content_page_id = c.content_page_id and icl.content_datasource_id = c.data_source_id 
-    //     inner join items i on i.item_page_id = icl.item_page_id and i.data_source_id = icl.item_datasource_id
-    //     where date is not null and mdl.map_page_id = ?
-    //     ${dataSourceIds ? 'and c.data_source_id in (?)' : ''}
-    //     union distinct 
-    //     select distinct icl.item_datasource_id as data_source_id, DATE_FORMAT(c.date, '%Y-%m-%d') as date, icl.content_page_id, icl.content_datasource_id from contents c 
-    //     inner join item_content_link icl on icl.content_page_id = c.content_page_id and icl.content_datasource_id = c.data_source_id 
-    //     inner join items i on i.item_page_id = icl.item_page_id and i.data_source_id = icl.item_datasource_id
-    //     inner join map_datasource_link mdl on mdl.data_source_id = c.data_source_id 
-    //     where date is not null and mdl.map_page_id = ?
-    //     ${dataSourceIds ? 'and icl.item_datasource_id in (?)' : ''}
-    //     `;
+        const sql = `
+        select c.date, c.data_id, d2.data_source_id from contents c 
+        inner join item_content_link icl on icl.content_data_id = c.data_id 
+        inner join datas d2 on d2.data_id = c.data_id 
+        -- 表示中の地図上のitemに紐づいているものに絞る
+        where EXISTS (
+            select * from datas d 
+            inner join data_source ds on d.data_source_id = ds.data_source_id 
+            inner join map_datasource_link mdl on mdl.data_source_id = d.data_source_id 
+            where mdl.map_page_id = ? and ds.location_kind in (?)
+            and d.data_id = icl.item_data_id 
+        )
+        and date is not NULL
+        `;
     
-    //     const params = dataSourceIds ?
-    //                         [currentMap.mapId, dataSourceIds, currentMap.mapId, dataSourceIds]
-    //                          : [currentMap.mapId, currentMap.mapId, currentMap.mapKind];
-    //     const query = con.format(sql, params);
-    //     const [rows] = await con.execute(query);
+        const locationKinds = currentMap.mapKind === MapKind.Real ? [DatasourceLocationKindType.RealItem, DatasourceLocationKindType.Track] : [DatasourceLocationKindType.VirtualItem];
+        const query = con.format(sql, [currentMap.mapId, locationKinds]);
+        const [rows] = await con.execute(query);
 
-    //     await con.commit();
+        await con.commit();
 
-    //     return (rows as DateResult[]);
+        if (!dataSourceIds) {
+            return (rows as DateResult[]);
+        } else {
+            return (rows as DateResult[]).filter(row => dataSourceIds.includes(row.data_source_id));
+        }
     
-    // } catch(e) {
-    //     logger.warn('get dates failed.', e);
-    //     await con.rollback();
-    //     throw new Error('get dates failed');
+    } catch(e) {
+        logger.warn('get dates failed.', e);
+        await con.rollback();
+        throw new Error('get dates failed');
 
-    // } finally {
-    //     con.release();
-    // }
+    } finally {
+        con.release();
+    }
 }
