d84198013d011c2eee3b855d77b0477a82c6d631
Satoko <satoko@suzuki.email.ne.jp>
2024-05-14 16:51:06 +0900

    publish core

--- 279map-backend-main/src/getItems.ts
+++ 279map-backend-main/src/getItems.ts
@@ -1,6 +1,5 @@
 import { getLogger } from 'log4js';
 import { ConnectionPool } from '.';
-import { PoolConnection } from 'mysql2/promise';
 import { CurrentMap } from '../279map-backend-common/src';
 import { GeometryItemsTable } from '../279map-backend-common/src/types/schema';
 import { QueryGetItemsArgs } from './graphql/__generated__/types';
