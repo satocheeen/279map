e9d55f593dc879e3691926fb1339957c52005e1b
Satoko <satoko@suzuki.email.ne.jp>
2024-11-25 15:47:51 +0900

    data_linkテーブルにfrom_field_key追加

--- 279map-db/init/1_ddl.sql
+++ 279map-db/init/1_ddl.sql
@@ -123,9 +123,10 @@ CREATE TABLE `images` (
 
 CREATE TABLE `data_link` (
   `from_data_id` varchar(50) NOT NULL,
+  `from_field_key` varchar(50) NOT NULL,
   `to_data_id` varchar(50) NOT NULL,
   `last_edited_time` varchar(100) NOT NULL,
-  PRIMARY KEY (`from_data_id`,`to_data_id`),
+  PRIMARY KEY (`from_data_id`,`from_field_key`, `to_data_id`),
   KEY `data_link_FK_1` (`to_data_id`),
   CONSTRAINT `data_link_FK` FOREIGN KEY (`from_data_id`) REFERENCES `datas` (`data_id`) ON DELETE CASCADE,
   CONSTRAINT `data_link_FK_1` FOREIGN KEY (`to_data_id`) REFERENCES `datas` (`data_id`) ON DELETE CASCADE
